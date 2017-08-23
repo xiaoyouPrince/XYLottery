4743ddbb0c9092d8527b8355f28496956ab75aab
渠晓友 <xiaoyouPrince@163.com>
2017-08-23 18:23:52 +0800

    Update XYNewsDetailViewController.m

--- XYLottery/XYLottery/Classes/News/Controller/XYNewsDetailViewController.m
+++ XYLottery/XYLottery/Classes/News/Controller/XYNewsDetailViewController.m
@@ -17,25 +17,6 @@
 @implementation XYNewsDetailViewController
 
 
-
-//- (NSDictionary *)titleDict
-//{
-//    if (_titleDict == nil) {
-//        _titleDict = @{@"ssq" : @"双色球",
-//                       @"dlt" : @"超级大乐透",
-//                       @"jczq" : @"竞彩足球",
-//                       @"jclq" : @"竞彩篮球",
-//                       @"zc" : @"足彩",
-//                       @"bjdc" : @"北京单场",
-//                       @"3d" : @"福彩3D",
-//                       @"pl3" : @"排列三",
-//                       };
-//        
-//    }
-//    return _titleDict;
-//}
-
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -44,43 +25,27 @@
     UIWebView *web = [UIWebView new];
     web.delegate = self;
     web.frame = self.view.bounds;
-    //    web.scrollView.contentInset = UIEdgeInsetsMake(0, 0, 65, 0);
     [self.view addSubview:web];
     self.web = web;
     [web loadRequest:self.request];
     
-//    self.title = self.titleDict[[self.request.URL lastPathComponent]];
 }
 
 
-//- (void)webViewDidFinishLoad:(UIWebView *)webView
-//{
-//    //    webView
-//    // 执行JS代码实现--这里移除头部，只是进来的时候移除一次，
-//    
-//    NSString *jsCode = @"var headerElement = document.getElementsByTagName('header')[0];"
-//    "headerElement.parentNode.removeChild(headerElement);";
-//    [webView stringByEvaluatingJavaScriptFromString:jsCode];
-//    
-//}
-//
-//
-//- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
-//{
-//    
-//    if ([request.URL.absoluteString containsString:@"html"]) {
-//        // 进入子页面
-//        // 即热点资讯页面
-////        HotInfoViewController *hot = [HotInfoViewController new];
-////        hot.request = request;
-////        [self.navigationController pushViewController:hot animated:YES];
-//        
-//        return NO;
-//    }
-//    
-//    return YES;
-//    
-//}
+- (void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    [SVProgressHUD dismiss];
+    
+}
+
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    
+    [SVProgressHUD show];
+    return YES;
+    
+}
 
 
 
